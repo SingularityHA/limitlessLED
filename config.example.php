8daeb70027b0316b5e19d13afd2b54903ec2925a
Dan Faulknor <daniel@zzrv.co>
2014-01-04 16:29:40 +1100

    Adding license notices

--- config.example.php
+++ config.example.php
@@ -1,4 +1,20 @@
 <?
+// Singularity
+// Copyright (C) 2014 Internet by Design Ltd
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 $hub[0]['name'] = "Lounge/Hallway";
 $hub[0]['ip'] = "172.18.1.3";
 $hub[0]['port'] = "8899";
